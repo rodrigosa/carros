772a0c990007e5f739ba852437bad2e64f3782fa
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-03-10 13:59:31 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/ratulinux/carros/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ratulinux.carros
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:carros/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, //Remover faixa de debug da tela
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white
+      ),
+      home: LoginPage()
+    );
+  }
+}
