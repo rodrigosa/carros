3d8ff9cc2c1161b1759b1d31afa9a81159895c86
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-03-18 16:02:12 -0300

    110. CarrosListView (Componente)

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:carros/drawer_list.dart';
 import 'package:carros/pages/carro/carro.dart';
 import 'package:carros/pages/carro/carros_api.dart';
+import 'package:carros/pages/carro/carros_listview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,99 +15,10 @@ class HomePage extends StatelessWidget {
           "Carros",
         ),
       )),
-      body: _body(),
+      body: CarrosListView(),
       drawer: DrawerList(),
     );
   }
 
-  _body() {
-    Future<List<Carro>> future = CarrosApi.getCarros();
 
-    return FutureBuilder(
-      future: future,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          //print(snapshot.error);
-          return Center(
-            child: Text(
-              "Não foi possível buscar os carros!",
-              style: TextStyle(color: Colors.red),
-            ),
-          );
-        }
-        if (!snapshot.hasData) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        List<Carro> carros = snapshot.data;
-        return _listView(carros);
-      },
-    );
-  }
-
-  Container _listView(List<Carro> carros) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      child: ListView.builder(
-          itemCount: carros != null ? carros.length : 0,
-          // teste para não dar erro quando no período do await caros estiver nulo
-          itemBuilder: (context, index) {
-            Carro c = carros[index];
-
-            return Card(
-              color: Colors.grey[100],
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Center(
-                      child: Image.network(
-                        c.urlFoto ?? "https://s3-sa-east-1.amazonaws.com/videos.livetouchdev.com.br/luxo/Shelby_Supercars_Ultimate.png", // se o valor for nulo ele pega esta url
-                        width: 250,
-                      ),
-                    ),
-                    Text(
-                      "${c.nome}", // Desta forma ignora se vier nulo
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    Text(
-                      "Descrição...",
-                      style: TextStyle(fontSize: 14),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        TextButton(
-                          child: const Text(
-                            'DETALHES',
-                            style: TextStyle(fontSize: 11),
-                          ),
-                          onPressed: () {
-                            /* ... */
-                          },
-                        ),
-                        const SizedBox(width: 8),
-                        TextButton(
-                          child: const Text(
-                            'SHARE',
-                            style: TextStyle(fontSize: 11),
-                          ),
-                          onPressed: () {
-                            /* ... */
-                          },
-                        ),
-                        const SizedBox(width: 8),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }),
-    );
-  }
 }
