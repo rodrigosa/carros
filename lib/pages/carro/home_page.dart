88f81e3b22cf4e2984b23e689d87dafd721d9e88
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-03-18 16:09:01 -0300

    111. TabBar (Classicos, esportivos e luxo)

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:carros/drawer_list.dart';
-import 'package:carros/pages/carro/carro.dart';
-import 'package:carros/pages/carro/carros_api.dart';
 import 'package:carros/pages/carro/carros_listview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,15 +6,30 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          title: Center(
-        child: Text(
-          "Carros",
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+            title: Center(
+          child: Text(
+            "Carros",
+          ),
         ),
-      )),
-      body: CarrosListView(),
-      drawer: DrawerList(),
+        bottom: TabBar(tabs: [
+          Tab(text: "Clássicos",),
+          Tab(text: "Esportivos",),
+          Tab(text: "Luxo",)
+        ],),
+        ),
+        body: TabBarView(
+          children: [
+            CarrosListView(),
+            CarrosListView(),
+            CarrosListView()
+          ],
+        ),
+        drawer: DrawerList(),
+      ),
     );
   }
 
