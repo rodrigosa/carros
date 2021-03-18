cb2641d8c6257144589190b98aca43918b47f787
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-03-18 10:45:08 -0300

    104. Layout com card

--- lib/pages/carro/home_page.dart
+++ lib/pages/carro/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:carros/drawer_list.dart';
 import 'package:carros/pages/carro/carro.dart';
 import 'package:carros/pages/carro/carros_api.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -21,24 +22,56 @@ class HomePage extends StatelessWidget {
   _body() {
     List<Carro> carros = CarrosApi.getCarros();
 
-    return ListView.builder(
-        itemCount: carros.length,
-        itemBuilder: (context, index) {
-          Carro c = carros[index];
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: ListView.builder(
+          itemCount: carros.length,
+          itemBuilder: (context, index) {
+            Carro c = carros[index];
 
-          return Row(
-            children: [
-              Image.network(c.urlFoto, width: 150,),
-              Flexible(
-                child: Text(
-                  c.nome,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 25),
+            return Card(
+              color: Colors.grey[100],
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Image.network(
+                        c.urlFoto,
+                        width: 250,
+                      ),
+                    ),
+                    Text(
+                      c.nome,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 25),
+                    ),
+                    Text(
+                      "Descrição...",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        TextButton(
+                          child: const Text('DETALHES', style: TextStyle(fontSize: 11),),
+                          onPressed: () { /* ... */ },
+                        ),
+                        const SizedBox(width: 8),
+                        TextButton(
+                          child: const Text('SHARE',style: TextStyle(fontSize: 11),),
+                          onPressed: () { /* ... */ },
+                        ),
+                        const SizedBox(width: 8),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ],
-          );
-        });
+            );
+          }),
+    );
   }
 }
